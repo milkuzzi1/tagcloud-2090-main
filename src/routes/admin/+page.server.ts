c5db602ee03e7b2e47858a568c6c424a08381d09
milkuzzi <yanemilkyzz@yandex.ru>
2026-06-04 07:05:32 +0300

    refactor: remove invites from admin page, only members needed

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,12 +1,9 @@
 import { requireAdmin } from '$lib/server/auth/access';
-import { listInvites, listMembers } from '$lib/server/auth/invites';
+import { listMembers } from '$lib/server/auth/invites';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
   requireAdmin(locals.user);
-  const [invites, members] = await Promise.all([
-    listInvites(),
-    listMembers()
-  ]);
-  return { invites, members, currentUserId: locals.user!.id };
+  const members = await listMembers();
+  return { members, currentUserId: locals.user!.id };
 };
